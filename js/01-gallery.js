9e9a594af4848c653ffa404898db27c1c9fcb5e7
ishkanderka <victoriashmidt@outlook.com>
2023-04-21 00:48:42 -0600

    Task-1 done ✔️

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const imageSrc = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+   <img width="1400" height="900" src="${imageSrc}">
+`
+  );
+
+  instance.show();
+}
+
+gallery.addEventListener("click", onGalleryClick);
